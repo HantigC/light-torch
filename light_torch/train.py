2ead31abf1cafc7ad8b246e8fdd0fa7814181cc6
HantigC <cristianhantig96@gmail.com>
2023-09-25 21:36:04 +0300

    add basic checkpoint and restoration for trainer

--- light_torch/train.py
+++ light_torch/train.py
@@ -1,16 +1,33 @@
+import os
+import torch
 from torch import nn
 from torch.utils.data.dataloader import DataLoader
 from tqdm.auto import tqdm
 
 from .module.base import Module
+from src.utils.init import default_if_none
 
 
 class Trainer(nn.Module):
-    def __init__(self, module: Module, accumulation=1):
+    def __init__(
+        self,
+        module: Module,
+        epochs=1,
+        accumulation=1,
+        optimizer=None,
+        save_dir="./",
+        name="trainer",
+    ):
         super().__init__()
+
+        self.optimizer = optimizer
         self.module = module
         self.accumulation = accumulation
         self.history = []
+        self.save_dir = save_dir
+        self.epochs = epochs
+        self.start_at_epoch = 1
+        self.name = name
 
     def train_one_epoch(self, data: DataLoader, epoch, optimizer=None):
         with self.module.on_train():
@@ -31,15 +48,41 @@ class Trainer(nn.Module):
                     tbar.set_postfix(logs)
                     tbar.update()
 
+    def save_checkpoint(self, epoch):
+        checkpoint = {
+            "start_at_epoch": epoch,
+            "epochs": self.epochs,
+            "optimizer": self.optimizer.state_dict(),
+            "module": self.module.state_dict(),
+            "save_dir": self.save_dir,
+        }
+        torch.save(checkpoint, self._make_pathname())
+
+    def _make_pathname(self):
+        return os.path.join(self.save_dir, f"{self.name}_last.zip")
+
+    def restore_checkpoint(self, save_dir, name=None):
+        self.save_dir = save_dir
+        name = default_if_none(name, self.name)
+
+        checkpoint = torch.load(self._make_pathname())
+        self.start_at_epoch = checkpoint["start_at_epoch"]
+        self.epochs = checkpoint["epochs"]
+        self.optimizer.load_state_dict(checkpoint["optimizer"])
+        self.module.load_state_dict(checkpoint["module"])
+        self.save_dir = checkpoint["save_dir"]
+
     def fit(
         self,
-        optimizer,
-        epochs: int,
         data: DataLoader,
+        optimizer: torch.optim.Optimizer = None,
+        epochs: int = None,
         val_data: DataLoader = None,
     ):
+        optimizer = default_if_none(optimizer, self.optimizer)
+        epochs = default_if_none(epochs, self.epochs)
         with self.module.on_fit():
-            for epoch in range(1, epochs + 1):
+            for epoch in range(self.start_at_epoch, epochs + 1):
                 self.train_one_epoch(data, epoch, optimizer)
                 train_metrics_report = self.module.get_report()
                 report = {"train": train_metrics_report, "epoch": epoch}
@@ -48,6 +91,7 @@ class Trainer(nn.Module):
                     eval_metrics_report = self.module.get_report()
                     report["eval"] = eval_metrics_report
                 self.history.append(report)
+                self.save_checkpoint(epoch + 1)
 
     def evaluate(self, data: DataLoader, epoch: int = None) -> None:
         with self.module.on_val():
